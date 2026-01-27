64a0f460c48361b06ffe7fad2811f5ad833e3aa8
Jack20 <chunjcsx20@vip.qq.com>
2026-01-27 22:00:34 +0800

    Define the member variables of the tiling structure

--- SigmoidCustom/op_host/sigmoid_custom_tiling.h
+++ SigmoidCustom/op_host/sigmoid_custom_tiling.h
@@ -3,7 +3,9 @@
 
 namespace optiling {
 BEGIN_TILING_DATA_DEF(SigmoidCustomTilingData)
-//考生自行定义tiling结构体成员变量
+//定义tiling结构体成员变量
+TILING_DATA_FIELD_DEF(uint32_t, totalLength);
+TILING_DATA_FIELD_DEF(uint32_t, tileNum);
 END_TILING_DATA_DEF;
 
 REGISTER_TILING_DATA_CLASS(SigmoidCustom, SigmoidCustomTilingData)
